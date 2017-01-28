de3ea18d29bbba1e543a766254e44620b3b1316c
Matthew Messinger <mattmess1221@gmail.com>
2017-01-28 01:27:29 -0500

    Add installer task and shadow mixin

--- src/main/java/zombe/launch/ZombeLaunchTweaker.java
+++ src/main/java/zombe/launch/ZombeLaunchTweaker.java
@@ -3,7 +3,7 @@ package zombe.launch;
 import java.io.File;
 import java.util.List;
 
-public class ZombeCascadingTweaker extends ZombeTweaker {
+public class ZombeLaunchTweaker extends ZombeTweaker {
 
     @Override
     public void acceptOptions(List<String> list, File gameDir, File assetsDir, String profile) {
